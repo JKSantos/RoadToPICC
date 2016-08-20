85f10f51e93403e82889adf2a87b58161ced37c7
Jeff <santos.jeffrey0023@gmail.com>
2016-08-20 11:36:28 +0800

    tangina working na reservation create git add -Agit add -A

---
+++ SalonManagement/src/com/gss/dao/ReportsJDBCRepository.java
@@ -0,0 +1,12 @@
+package com.gss.dao;
+
+import com.gss.connection.JDBCConnection;
+
+public interface ReportsJDBCRepository {
+	
+	JDBCConnection jdbc = new JDBCConnection();
+	
+	
+	public List<>
+
+}
